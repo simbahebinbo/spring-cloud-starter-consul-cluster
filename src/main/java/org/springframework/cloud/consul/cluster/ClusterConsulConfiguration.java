0fc2a54003a6954520b09d18613eb212f470a611
penghuaping <penghuaping@huobi.com>
2020-02-26 21:59:04 +0800

    修改一个包

--- src/main/java/org/springframework/cloud/consul/cluster/ClusterConsulConfiguration.java
+++ src/main/java/org/springframework/cloud/consul/cluster/ClusterConsulConfiguration.java
@@ -9,11 +9,11 @@ import javax.annotation.PostConstruct;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.consul.ConditionalOnConsulEnabled;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.util.CollectionUtils;
 
 @Slf4j
 @Configuration
